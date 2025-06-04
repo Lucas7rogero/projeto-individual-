b93e91eeca468e857e56099c7cdd38466089e7b6
Lucas7rogero <lucas.rogero@sou.inteli.edu.br>
2025-06-04 18:50:54 -0300

    fix: finaliza o css

--- src/public/js/login.js
+++
@@ -1,5 +0,0 @@
-function salvarId() {
-    const nome = document.querySelector('input[name="nome"]').value;
-    localStorage.setItem('username', nome);
-    console.log('Usuário salvo no localStorage:', nome);
-}
