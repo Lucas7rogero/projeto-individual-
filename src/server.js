235f7f5493430dbb6ddabc10566e594b65520808
Lucas7rogero <lucas.rogero@sou.inteli.edu.br>
2025-06-05 15:33:42 -0300

    fix: codio completo rodando perfeito

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
 
